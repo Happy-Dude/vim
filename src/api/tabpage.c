tarruda/neovim

Refactor API types and prototypes
- Split functions with multiple files in the 'api' subdirectory
- Move/Add more types in the 'api/defs.h' header
- Add more prototypes
- Refactor scripts/msgpack-gen.lua
- Move msgpack modules to 'os' subdirectory

src/api/tabpage.c
@@ -1,0 +1,32 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "api/tabpage.h"
+#include "api/defs.h"
+
+uint64_t tabpage_get_window_count(Tabpage tabpage, Error *err)
+{
+  abort();
+}
+
+Object tabpage_get_var(Tabpage tabpage, String name, Error *err)
+{
+  abort();
+}
+
+void tabpage_set_var(Tabpage tabpage, String name, Object value, Error *err)
+{
+  abort();
+}
+
+Window tabpage_get_buffer(Tabpage tabpage, Error *err)
+{
+  abort();
+}
+
+bool tabpage_is_valid(Tabpage tabpage)
+{
+  abort();
+}
+

src/api/tabpage.h
@@ -1,0 +1,46 @@
+#ifndef NEOVIM_API_TABPAGE_H
+#define NEOVIM_API_TABPAGE_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "api/defs.h"
+
+/// Gets the number of windows in a tabpage
+///
+/// @param tabpage The tabpage
+/// @param[out] err Details of an error that may have occurred
+/// @return The number of windows in `tabpage`
+uint64_t tabpage_get_window_count(Tabpage tabpage, Error *err);
+
+/// Gets a tabpage variable
+///
+/// @param tabpage The tab page handle
+/// @param name The variable name
+/// @param[out] err Details of an error that may have occurred
+/// @return The variable value
+Object tabpage_get_var(Tabpage tabpage, String name, Error *err);
+
+/// Sets a tabpage variable
+///
+/// @param tabpage andle
+/// @param name The variable name
+/// @param value The variable value
+/// @param[out] err Details of an error that may have occurred
+void tabpage_set_var(Tabpage tabpage, String name, Object value, Error *err);
+
+/// Gets the current window in a tab page
+///
+/// @param tabpage The tab page handle
+/// @param[out] err Details of an error that may have occurred
+/// @return The Window handle
+Window tabpage_get_buffer(Tabpage tabpage, Error *err);
+
+/// Checks if a tab page is valid
+///
+/// @param tabpage The tab page handle
+/// @return true if the tab page is valid, false otherwise
+bool tabpage_is_valid(Tabpage tabpage);
+
+#endif // NEOVIM_API_TABPAGE_H
+

src/api/vim.c
@@ -1,0 +1,140 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "api/vim.h"
+#include "api/defs.h"
+
+void vim_push_keys(String str)
+{
+  abort();
+}
+
+void vim_command(String str, Error *err)
+{
+  abort();
+}
+
+Object vim_eval(String str, Error *err)
+{
+  abort();
+}
+
+int64_t vim_strwidth(String str)
+{
+  abort();
+}
+
+StringArray vim_list_runtime_paths(void)
+{
+  abort();
+}
+
+void vim_change_directory(String dir)
+{
+  abort();
+}
+
+String vim_get_current_line(void)
+{
+  abort();
+}
+
+void vim_set_current_line(String line)
+{
+  abort();
+}
+
+Object vim_get_var(bool special, String name, Error *err)
+{
+  abort();
+}
+
+void vim_set_var(bool special, String name, Object value, Error *err)
+{
+  abort();
+}
+
+String vim_get_option(String name, Error *err)
+{
+  abort();
+}
+
+void vim_set_option(String name, String value, Error *err)
+{
+  abort();
+}
+
+void vim_del_option(String name, Error *err)
+{
+  abort();
+}
+
+void vim_out_write(String str)
+{
+  abort();
+}
+
+void vim_err_write(String str)
+{
+  abort();
+}
+
+uint64_t vim_get_buffer_count(void)
+{
+  abort();
+}
+
+Buffer vim_get_buffer(int64_t num, Error *err)
+{
+  abort();
+}
+
+Buffer vim_get_current_buffer(void)
+{
+  abort();
+}
+
+void vim_set_current_buffer(Buffer buffer)
+{
+  abort();
+}
+
+uint64_t vim_get_window_count(void)
+{
+  abort();
+}
+
+Window vim_get_window(int64_t num, Error *err)
+{
+  abort();
+}
+
+Window vim_get_current_window(void)
+{
+  abort();
+}
+
+void vim_set_current_window(Window window)
+{
+  abort();
+}
+
+uint64_t vim_get_tabpage_count(void)
+{
+  abort();
+}
+
+Tabpage vim_get_tabpage(int64_t num, Error *err)
+{
+  abort();
+}
+
+Tabpage vim_get_current_tabpage(void)
+{
+  abort();
+}
+
+void vim_set_current_tabpage(Tabpage tabpage)
+{
+  abort();
+}

src/api/vim.h
@@ -1,0 +1,172 @@
+#ifndef NEOVIM_API_VIM_H
+#define NEOVIM_API_VIM_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "api/defs.h"
+
+/// Send keys to vim input buffer, simulating user input.
+///
+/// @param str The keys to send
+void vim_push_keys(String str);
+
+/// Executes an ex-mode command str
+///
+/// @param str The command str
+/// @param[out] err Details of an error that may have occurred
+void vim_command(String str, Error *err);
+
+/// Evaluates the expression str using the vim internal expression
+/// evaluator (see |expression|).  Returns the expression result as:
+/// - a string if the Vim expression evaluates to a string or number
+/// - a list if the Vim expression evaluates to a Vim list
+/// - a dictionary if the Vim expression evaluates to a Vim dictionary
+/// Dictionaries and lists are recursively expanded.
+///
+/// @param str The expression str
+/// @param[out] err Details of an error that may have occurred
+/// @return The expanded object
+Object vim_eval(String str, Error *err);
+
+/// Calculates the number of display cells `str` occupies, tab is counted as
+/// one cell.
+///
+/// @param str Some text
+/// @return The number of cells
+int64_t vim_strwidth(String str);
+
+/// Returns a list of paths contained in 'runtimepath'
+/// 
+/// @return The list of paths
+StringArray vim_list_runtime_paths(void);
+
+/// Changes vim working directory
+///
+/// @param dir The new working directory
+void vim_change_directory(String dir);
+
+/// Return the current line
+///
+/// @return The current line string
+String vim_get_current_line(void);
+
+/// Sets the current line
+///
+/// @param line The line contents
+void vim_set_current_line(String line);
+
+/// Gets a global or special variable
+///
+/// @param special If it's a special(:v) variable
+/// @param name The variable name
+/// @param[out] err Details of an error that may have occurred
+/// @return The variable value
+Object vim_get_var(bool special, String name, Error *err);
+
+/// Sets a global or special variable
+///
+/// @param special If it's a special(:v) variable
+/// @param name The variable name
+/// @param value The variable value
+/// @param[out] err Details of an error that may have occurred
+void vim_set_var(bool special, String name, Object value, Error *err);
+
+/// Get an option value string
+///
+/// @param name The option name
+/// @param[out] err Details of an error that may have occurred
+/// @return The option value
+String vim_get_option(String name, Error *err);
+
+/// Sets an option value
+///
+/// @param name The option name
+/// @param value The new option value
+/// @param[out] err Details of an error that may have occurred
+void vim_set_option(String name, String value, Error *err);
+
+/// Deletes an option, falling back to the default value
+///
+/// @param name The option name
+/// @param[out] err Details of an error that may have occurred
+void vim_del_option(String name, Error *err);
+
+/// Write a message to vim output buffer
+///
+/// @param str The message
+void vim_out_write(String str);
+
+/// Write a message to vim error buffer
+///
+/// @param str The message
+void vim_err_write(String str);
+
+/// Gets the number of buffers
+///
+/// @return The number of buffers
+uint64_t vim_get_buffer_count(void);
+
+/// Gets a buffer by index
+///
+/// @param num The buffer number
+/// @param[out] err Details of an error that may have occurred
+/// @return The buffer handle
+Buffer vim_get_buffer(int64_t num, Error *err);
+
+/// Return the current buffer
+///
+/// @reqturn The buffer handle
+Buffer vim_get_current_buffer(void);
+
+/// Sets the current buffer
+///
+/// @param id The buffer handle
+void vim_set_current_buffer(Buffer buffer);
+
+/// Gets the number of windows
+///
+/// @return The number of windows
+uint64_t vim_get_window_count(void);
+
+/// Gets a window by index
+///
+/// @param num The window number
+/// @param[out] err Details of an error that may have occurred
+/// @return The window handle
+Window vim_get_window(int64_t num, Error *err);
+
+/// Return the current window
+///
+/// @return The window handle
+Window vim_get_current_window(void);
+
+/// Sets the current window
+///
+/// @param handle The window handle
+void vim_set_current_window(Window window);
+
+/// Gets the number of tab pages
+///
+/// @return The number of tab pages
+uint64_t vim_get_tabpage_count(void);
+
+/// Gets a tab page by index
+///
+/// @param num The tabpage number
+/// @param[out] err Details of an error that may have occurred
+/// @return The tab page handle
+Tabpage vim_get_tabpage(int64_t num, Error *err);
+
+/// Return the current tab page
+///
+/// @return The tab page handle
+Tabpage vim_get_current_tabpage(void);
+
+/// Sets the current tab page
+///
+/// @param handle The tab page handle
+void vim_set_current_tabpage(Tabpage tabpage);
+
+#endif // NEOVIM_API_VIM_H
+

src/api/window.c
@@ -1,0 +1,73 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "api/window.h"
+#include "api/defs.h"
+
+
+Buffer window_get_buffer(Window window, Error *err)
+{
+  abort();
+}
+
+Position window_get_cursor(Window window, Error *err)
+{
+  abort();
+}
+
+void window_set_cursor(Window window, Position pos, Error *err)
+{
+  abort();
+}
+
+uint64_t window_get_height(Window window, Error *err)
+{
+  abort();
+}
+
+void window_set_height(Window window, uint64_t height, Error *err)
+{
+  abort();
+}
+
+uint64_t window_get_width(Window window, Error *err)
+{
+  abort();
+}
+
+Object window_get_var(Window window, String name, Error *err)
+{
+  abort();
+}
+
+void window_set_var(Window window, String name, Object value, Error *err)
+{
+  abort();
+}
+
+String window_get_option(Window window, String name, Error *err)
+{
+  abort();
+}
+
+void window_set_option(Window window, String name, String value, Error *err)
+{
+  abort();
+}
+
+Position window_get_pos(Window window, Error *err)
+{
+  abort();
+}
+
+Tabpage window_get_tabpage(Window window, Error *err)
+{
+  abort();
+}
+
+bool window_is_valid(Window window)
+{
+  abort();
+}
+

src/api/window.h
@@ -1,0 +1,105 @@
+#ifndef NEOVIM_API_WINDOW_H
+#define NEOVIM_API_WINDOW_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "api/defs.h"
+
+/// Gets the current buffer in a window
+///
+/// @param window The window handle
+/// @param[out] err Details of an error that may have occurred
+/// @return The buffer handle
+Buffer window_get_buffer(Window window, Error *err);
+
+/// Gets the cursor position in the window
+///
+/// @param window The window handle
+/// @param[out] err Details of an error that may have occurred
+/// @return the (row, col) tuple
+Position window_get_cursor(Window window, Error *err);
+
+/// Sets the cursor position in the window
+///
+/// @param window The window handle
+/// @param pos the (row, col) tuple representing the new position
+/// @param[out] err Details of an error that may have occurred
+void window_set_cursor(Window window, Position pos, Error *err);
+
+/// Gets the window height
+///
+/// @param window The window handle
+/// @param[out] err Details of an error that may have occurred
+/// @return the height in rows
+uint64_t window_get_height(Window window, Error *err);
+
+/// Sets the window height. This will only succeed if the screen is split
+/// horizontally.
+///
+/// @param window The window handle
+/// @param height the new height in rows
+/// @param[out] err Details of an error that may have occurred
+void window_set_height(Window window, uint64_t height, Error *err);
+
+/// Gets the window width
+///
+/// @param window The window handle
+/// @param[out] err Details of an error that may have occurred
+/// @return the width in columns
+uint64_t window_get_width(Window window, Error *err);
+
+/// Gets a window variable
+///
+/// @param window The window handle
+/// @param name The variable name
+/// @param[out] err Details of an error that may have occurred
+/// @return The variable value
+Object window_get_var(Window window, String name, Error *err);
+
+/// Sets a window variable
+///
+/// @param window The window handle
+/// @param name The variable name
+/// @param value The variable value
+/// @param[out] err Details of an error that may have occurred
+void window_set_var(Window window, String name, Object value, Error *err);
+
+/// Gets a window option value
+///
+/// @param window The window handle
+/// @param name The option name
+/// @param[out] err Details of an error that may have occurred
+/// @return The option value
+String window_get_option(Window window, String name, Error *err);
+
+/// Sets a window option value
+///
+/// @param window The window handle
+/// @param name The option name
+/// @param value The option value
+/// @param[out] err Details of an error that may have occurred
+void window_set_option(Window window, String name, String value, Error *err);
+
+/// Gets the window position in display cells. First position is zero.
+///
+/// @param window The window handle
+/// @param[out] err Details of an error that may have occurred
+/// @return The (row, col) tuple with the window position
+Position window_get_pos(Window window, Error *err);
+
+/// Gets the window tab page
+/// 
+/// @param window The window handle
+/// @param[out] err Details of an error that may have occurred
+/// @return The tab page that contains the window
+Tabpage window_get_tabpage(Window window, Error *err);
+
+/// Checks if a window is valid
+///
+/// @param window The window handle
+/// @return true if the window is valid, false otherwise
+bool window_is_valid(Window window);
+
+#endif // NEOVIM_API_WINDOW_H
+

Happy-Dude/vim

src/api/tabpage.c
@@ -1,0 +1,32 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "api/tabpage.h"
+#include "api/defs.h"
+
+int64_t tabpage_get_window_count(Tabpage tabpage, Error *err)
+{
+  abort();
+}
+
+Object tabpage_get_var(Tabpage tabpage, String name, Error *err)
+{
+  abort();
+}
+
+void tabpage_set_var(Tabpage tabpage, String name, Object value, Error *err)
+{
+  abort();
+}
+
+Window tabpage_get_buffer(Tabpage tabpage, Error *err)
+{
+  abort();
+}
+
+bool tabpage_is_valid(Tabpage tabpage)
+{
+  abort();
+}
+

src/api/tabpage.h
@@ -1,0 +1,46 @@
+#ifndef NEOVIM_API_TABPAGE_H
+#define NEOVIM_API_TABPAGE_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "api/defs.h"
+
+/// Gets the number of windows in a tabpage
+///
+/// @param tabpage The tabpage
+/// @param[out] err Details of an error that may have occurred
+/// @return The number of windows in `tabpage`
+int64_t tabpage_get_window_count(Tabpage tabpage, Error *err);
+
+/// Gets a tabpage variable
+///
+/// @param tabpage The tab page handle
+/// @param name The variable name
+/// @param[out] err Details of an error that may have occurred
+/// @return The variable value
+Object tabpage_get_var(Tabpage tabpage, String name, Error *err);
+
+/// Sets a tabpage variable
+///
+/// @param tabpage andle
+/// @param name The variable name
+/// @param value The variable value
+/// @param[out] err Details of an error that may have occurred
+void tabpage_set_var(Tabpage tabpage, String name, Object value, Error *err);
+
+/// Gets the current window in a tab page
+///
+/// @param tabpage The tab page handle
+/// @param[out] err Details of an error that may have occurred
+/// @return The Window handle
+Window tabpage_get_buffer(Tabpage tabpage, Error *err);
+
+/// Checks if a tab page is valid
+///
+/// @param tabpage The tab page handle
+/// @return true if the tab page is valid, false otherwise
+bool tabpage_is_valid(Tabpage tabpage);
+
+#endif // NEOVIM_API_TABPAGE_H
+

src/api/vim.c
@@ -1,0 +1,140 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "api/vim.h"
+#include "api/defs.h"
+
+void vim_push_keys(String str)
+{
+  abort();
+}
+
+void vim_command(String str, Error *err)
+{
+  abort();
+}
+
+Object vim_eval(String str, Error *err)
+{
+  abort();
+}
+
+int64_t vim_strwidth(String str)
+{
+  abort();
+}
+
+StringArray vim_list_runtime_paths(void)
+{
+  abort();
+}
+
+void vim_change_directory(String dir)
+{
+  abort();
+}
+
+String vim_get_current_line(void)
+{
+  abort();
+}
+
+void vim_set_current_line(String line)
+{
+  abort();
+}
+
+Object vim_get_var(bool special, String name, Error *err)
+{
+  abort();
+}
+
+void vim_set_var(bool special, String name, Object value, Error *err)
+{
+  abort();
+}
+
+String vim_get_option(String name, Error *err)
+{
+  abort();
+}
+
+void vim_set_option(String name, String value, Error *err)
+{
+  abort();
+}
+
+void vim_del_option(String name, Error *err)
+{
+  abort();
+}
+
+void vim_out_write(String str)
+{
+  abort();
+}
+
+void vim_err_write(String str)
+{
+  abort();
+}
+
+int64_t vim_get_buffer_count(void)
+{
+  abort();
+}
+
+Buffer vim_get_buffer(int64_t num, Error *err)
+{
+  abort();
+}
+
+Buffer vim_get_current_buffer(void)
+{
+  abort();
+}
+
+void vim_set_current_buffer(Buffer buffer)
+{
+  abort();
+}
+
+int64_t vim_get_window_count(void)
+{
+  abort();
+}
+
+Window vim_get_window(int64_t num, Error *err)
+{
+  abort();
+}
+
+Window vim_get_current_window(void)
+{
+  abort();
+}
+
+void vim_set_current_window(Window window)
+{
+  abort();
+}
+
+int64_t vim_get_tabpage_count(void)
+{
+  abort();
+}
+
+Tabpage vim_get_tabpage(int64_t num, Error *err)
+{
+  abort();
+}
+
+Tabpage vim_get_current_tabpage(void)
+{
+  abort();
+}
+
+void vim_set_current_tabpage(Tabpage tabpage)
+{
+  abort();
+}

src/api/vim.h
@@ -1,0 +1,172 @@
+#ifndef NEOVIM_API_VIM_H
+#define NEOVIM_API_VIM_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "api/defs.h"
+
+/// Send keys to vim input buffer, simulating user input.
+///
+/// @param str The keys to send
+void vim_push_keys(String str);
+
+/// Executes an ex-mode command str
+///
+/// @param str The command str
+/// @param[out] err Details of an error that may have occurred
+void vim_command(String str, Error *err);
+
+/// Evaluates the expression str using the vim internal expression
+/// evaluator (see |expression|).  Returns the expression result as:
+/// - a string if the Vim expression evaluates to a string or number
+/// - a list if the Vim expression evaluates to a Vim list
+/// - a dictionary if the Vim expression evaluates to a Vim dictionary
+/// Dictionaries and lists are recursively expanded.
+///
+/// @param str The expression str
+/// @param[out] err Details of an error that may have occurred
+/// @return The expanded object
+Object vim_eval(String str, Error *err);
+
+/// Calculates the number of display cells `str` occupies, tab is counted as
+/// one cell.
+///
+/// @param str Some text
+/// @return The number of cells
+int64_t vim_strwidth(String str);
+
+/// Returns a list of paths contained in 'runtimepath'
+/// 
+/// @return The list of paths
+StringArray vim_list_runtime_paths(void);
+
+/// Changes vim working directory
+///
+/// @param dir The new working directory
+void vim_change_directory(String dir);
+
+/// Return the current line
+///
+/// @return The current line string
+String vim_get_current_line(void);
+
+/// Sets the current line
+///
+/// @param line The line contents
+void vim_set_current_line(String line);
+
+/// Gets a global or special variable
+///
+/// @param special If it's a special(:v) variable
+/// @param name The variable name
+/// @param[out] err Details of an error that may have occurred
+/// @return The variable value
+Object vim_get_var(bool special, String name, Error *err);
+
+/// Sets a global or special variable
+///
+/// @param special If it's a special(:v) variable
+/// @param name The variable name
+/// @param value The variable value
+/// @param[out] err Details of an error that may have occurred
+void vim_set_var(bool special, String name, Object value, Error *err);
+
+/// Get an option value string
+///
+/// @param name The option name
+/// @param[out] err Details of an error that may have occurred
+/// @return The option value
+String vim_get_option(String name, Error *err);
+
+/// Sets an option value
+///
+/// @param name The option name
+/// @param value The new option value
+/// @param[out] err Details of an error that may have occurred
+void vim_set_option(String name, String value, Error *err);
+
+/// Deletes an option, falling back to the default value
+///
+/// @param name The option name
+/// @param[out] err Details of an error that may have occurred
+void vim_del_option(String name, Error *err);
+
+/// Write a message to vim output buffer
+///
+/// @param str The message
+void vim_out_write(String str);
+
+/// Write a message to vim error buffer
+///
+/// @param str The message
+void vim_err_write(String str);
+
+/// Gets the number of buffers
+///
+/// @return The number of buffers
+int64_t vim_get_buffer_count(void);
+
+/// Gets a buffer by index
+///
+/// @param num The buffer number
+/// @param[out] err Details of an error that may have occurred
+/// @return The buffer handle
+Buffer vim_get_buffer(int64_t num, Error *err);
+
+/// Return the current buffer
+///
+/// @reqturn The buffer handle
+Buffer vim_get_current_buffer(void);
+
+/// Sets the current buffer
+///
+/// @param id The buffer handle
+void vim_set_current_buffer(Buffer buffer);
+
+/// Gets the number of windows
+///
+/// @return The number of windows
+int64_t vim_get_window_count(void);
+
+/// Gets a window by index
+///
+/// @param num The window number
+/// @param[out] err Details of an error that may have occurred
+/// @return The window handle
+Window vim_get_window(int64_t num, Error *err);
+
+/// Return the current window
+///
+/// @return The window handle
+Window vim_get_current_window(void);
+
+/// Sets the current window
+///
+/// @param handle The window handle
+void vim_set_current_window(Window window);
+
+/// Gets the number of tab pages
+///
+/// @return The number of tab pages
+int64_t vim_get_tabpage_count(void);
+
+/// Gets a tab page by index
+///
+/// @param num The tabpage number
+/// @param[out] err Details of an error that may have occurred
+/// @return The tab page handle
+Tabpage vim_get_tabpage(int64_t num, Error *err);
+
+/// Return the current tab page
+///
+/// @return The tab page handle
+Tabpage vim_get_current_tabpage(void);
+
+/// Sets the current tab page
+///
+/// @param handle The tab page handle
+void vim_set_current_tabpage(Tabpage tabpage);
+
+#endif // NEOVIM_API_VIM_H
+

src/api/window.c
@@ -1,0 +1,73 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "api/window.h"
+#include "api/defs.h"
+
+
+Buffer window_get_buffer(Window window, Error *err)
+{
+  abort();
+}
+
+Position window_get_cursor(Window window, Error *err)
+{
+  abort();
+}
+
+void window_set_cursor(Window window, Position pos, Error *err)
+{
+  abort();
+}
+
+int64_t window_get_height(Window window, Error *err)
+{
+  abort();
+}
+
+void window_set_height(Window window, int64_t height, Error *err)
+{
+  abort();
+}
+
+int64_t window_get_width(Window window, Error *err)
+{
+  abort();
+}
+
+Object window_get_var(Window window, String name, Error *err)
+{
+  abort();
+}
+
+void window_set_var(Window window, String name, Object value, Error *err)
+{
+  abort();
+}
+
+String window_get_option(Window window, String name, Error *err)
+{
+  abort();
+}
+
+void window_set_option(Window window, String name, String value, Error *err)
+{
+  abort();
+}
+
+Position window_get_pos(Window window, Error *err)
+{
+  abort();
+}
+
+Tabpage window_get_tabpage(Window window, Error *err)
+{
+  abort();
+}
+
+bool window_is_valid(Window window)
+{
+  abort();
+}
+

src/api/window.h
@@ -1,0 +1,105 @@
+#ifndef NEOVIM_API_WINDOW_H
+#define NEOVIM_API_WINDOW_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "api/defs.h"
+
+/// Gets the current buffer in a window
+///
+/// @param window The window handle
+/// @param[out] err Details of an error that may have occurred
+/// @return The buffer handle
+Buffer window_get_buffer(Window window, Error *err);
+
+/// Gets the cursor position in the window
+///
+/// @param window The window handle
+/// @param[out] err Details of an error that may have occurred
+/// @return the (row, col) tuple
+Position window_get_cursor(Window window, Error *err);
+
+/// Sets the cursor position in the window
+///
+/// @param window The window handle
+/// @param pos the (row, col) tuple representing the new position
+/// @param[out] err Details of an error that may have occurred
+void window_set_cursor(Window window, Position pos, Error *err);
+
+/// Gets the window height
+///
+/// @param window The window handle
+/// @param[out] err Details of an error that may have occurred
+/// @return the height in rows
+int64_t window_get_height(Window window, Error *err);
+
+/// Sets the window height. This will only succeed if the screen is split
+/// horizontally.
+///
+/// @param window The window handle
+/// @param height the new height in rows
+/// @param[out] err Details of an error that may have occurred
+void window_set_height(Window window, int64_t height, Error *err);
+
+/// Gets the window width
+///
+/// @param window The window handle
+/// @param[out] err Details of an error that may have occurred
+/// @return the width in columns
+int64_t window_get_width(Window window, Error *err);
+
+/// Gets a window variable
+///
+/// @param window The window handle
+/// @param name The variable name
+/// @param[out] err Details of an error that may have occurred
+/// @return The variable value
+Object window_get_var(Window window, String name, Error *err);
+
+/// Sets a window variable
+///
+/// @param window The window handle
+/// @param name The variable name
+/// @param value The variable value
+/// @param[out] err Details of an error that may have occurred
+void window_set_var(Window window, String name, Object value, Error *err);
+
+/// Gets a window option value
+///
+/// @param window The window handle
+/// @param name The option name
+/// @param[out] err Details of an error that may have occurred
+/// @return The option value
+String window_get_option(Window window, String name, Error *err);
+
+/// Sets a window option value
+///
+/// @param window The window handle
+/// @param name The option name
+/// @param value The option value
+/// @param[out] err Details of an error that may have occurred
+void window_set_option(Window window, String name, String value, Error *err);
+
+/// Gets the window position in display cells. First position is zero.
+///
+/// @param window The window handle
+/// @param[out] err Details of an error that may have occurred
+/// @return The (row, col) tuple with the window position
+Position window_get_pos(Window window, Error *err);
+
+/// Gets the window tab page
+/// 
+/// @param window The window handle
+/// @param[out] err Details of an error that may have occurred
+/// @return The tab page that contains the window
+Tabpage window_get_tabpage(Window window, Error *err);
+
+/// Checks if a window is valid
+///
+/// @param window The window handle
+/// @return true if the window is valid, false otherwise
+bool window_is_valid(Window window);
+
+#endif // NEOVIM_API_WINDOW_H
+