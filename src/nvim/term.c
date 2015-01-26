neovim/neovim

commit 04c0658024a98a0586997f0ea8af1e3f774cc83e

Cleanup: Refactor getdigits().
Problem  : getdigits() currently returns a long, but at most places,
           return value is casted (unsafely) into an int. Making casts
           safe would introduce a lot of fuss in the form of assertions
           checking for limits.
Note     : We cannot just change return type to int, because, at some
           places, legitimate long values are used. For example, in
           diff.c, for line numbers.
Solution : Introduce new functions:
           - get_digits()      : Gets an intmax_t from a string.
           - get_int_digits()  : Wrapper for ints.
           - get_long_digits() : Wrapper for longs.
           And replace getdigits() invocations by the appropiate
           wrapper invocations.

src/nvim/term.c
@@ -3230,7 +3230,7 @@ int check_termcode(int max_offset, char_u *buf, int bufsize, int *buflen)
            */
           p = tp + slen;
 
-          mouse_code = getdigits(&p);
+          mouse_code = get_long_digits(&p);
           if (*p++ != ';')
             return -1;
 
@@ -3238,15 +3238,11 @@ int check_termcode(int max_offset, char_u *buf, int bufsize, int *buflen)
           if (key_name[0] == KS_SGR_MOUSE)
             mouse_code += 32;
 
-          long digits = getdigits(&p);
-          assert(digits >= INT_MIN && digits <= INT_MAX);
-          mouse_col = (int)digits - 1;
+          mouse_col = get_int_digits(&p);
           if (*p++ != ';')
             return -1;
 
-          digits = getdigits(&p);
-          assert(digits >= INT_MIN && digits <= INT_MAX);
-          mouse_row = (int)digits - 1;
+          mouse_row = get_int_digits(&p);
           if (key_name[0] == KS_SGR_MOUSE && *p == 'm')
             mouse_code |= MOUSE_RELEASE;
           else if (*p != 'M')
@@ -3273,7 +3269,7 @@ int check_termcode(int max_offset, char_u *buf, int bufsize, int *buflen)
               }
             }
             p += j;
-            if (cmd_complete && getdigits(&p) == mouse_code) {
+            if (cmd_complete && get_long_digits(&p) == mouse_code) {
               slen += j;               /* skip the \033[ */
               continue;
             }
@@ -3319,10 +3315,10 @@ int check_termcode(int max_offset, char_u *buf, int bufsize, int *buflen)
          * '6' is the row, 45 is the column
          */
         p = tp + slen;
-        mr = getdigits(&p);
+        mr = get_long_digits(&p);
         if (*p++ != ',')
           return -1;
-        mc = getdigits(&p);
+        mc = get_long_digits(&p);
         if (*p++ != '\r')
           return -1;
 
@@ -3389,27 +3385,27 @@ int check_termcode(int max_offset, char_u *buf, int bufsize, int *buflen)
         p = tp + slen;
 
         /* get event status */
-        Pe = getdigits(&p);
+        Pe = get_long_digits(&p);
         if (*p++ != ';')
           return -1;
 
         /* get button status */
-        Pb = getdigits(&p);
+        Pb = get_long_digits(&p);
         if (*p++ != ';')
           return -1;
 
         /* get row status */
-        Pr = getdigits(&p);
+        Pr = get_long_digits(&p);
         if (*p++ != ';')
           return -1;
 
         /* get column status */
-        Pc = getdigits(&p);
+        Pc = get_long_digits(&p);
 
         /* the page parameter is optional */
         if (*p == ';') {
           p++;
-          (void)getdigits(&p);
+          (void)get_long_digits(&p);
         }
         if (*p++ != '&')
           return -1;

src/nvim/buffer.c
@@ -750,7 +750,7 @@ do_bufdel (
             break;
           arg = p;
         } else
-          bnr = getdigits(&arg);
+          bnr = get_int_digits(&arg);
       }
     }
     if (!got_int && do_current && do_buffer(command, DOBUF_FIRST,
@@ -2997,7 +2997,7 @@ build_stl_str_hl (
       l = -1;
     }
     if (VIM_ISDIGIT(*s)) {
-      minwid = (int)getdigits(&s);
+      minwid = get_int_digits(&s);
       if (minwid < 0)           /* overflow */
         minwid = 0;
     }
@@ -3033,7 +3033,7 @@ build_stl_str_hl (
     if (*s == '.') {
       s++;
       if (VIM_ISDIGIT(*s)) {
-        maxwid = (int)getdigits(&s);
+        maxwid = get_int_digits(&s);
         if (maxwid <= 0)                /* overflow */
           maxwid = 50;
       }
@@ -4077,7 +4077,7 @@ chk_modeline (
           e = s + 4;
         else
           e = s + 3;
-        vers = getdigits(&e);
+        vers = get_int_digits(&e);
         if (*e == ':'
             && (s[0] != 'V'
                 || STRNCMP(skipwhite(e + 1), "set", 3) == 0)

src/nvim/charset.c
@@ -177,7 +177,7 @@ int buf_init_chartab(buf_T *buf, int global)
       }
 
       if (VIM_ISDIGIT(*p)) {
-        c = getdigits(&p);
+        c = get_int_digits(&p);
       } else if (has_mbyte) {
         c = mb_ptr2char_adv(&p);
       } else {
@@ -189,7 +189,7 @@ int buf_init_chartab(buf_T *buf, int global)
         ++p;
 
         if (VIM_ISDIGIT(*p)) {
-          c2 = getdigits(&p);
+          c2 = get_int_digits(&p);
         } else if (has_mbyte) {
           c2 = mb_ptr2char_adv(&p);
         } else {
@@ -1676,26 +1676,37 @@ char_u* skiptowhite_esc(char_u *p) {
   return p;
 }
 
-/// Getdigits: Get a number from a string and skip over it.
+/// Get a number from a string and skip over it.
 ///
-/// Note: the argument is a pointer to a char_u pointer!
+/// @param[out]  pp  A pointer to a pointer to char_u.
+///                  It will be advanced past the read number.
 ///
-/// @param pp
-///
-/// @return Number from the string.
-long getdigits(char_u **pp)
+/// @return Number read from the string.
+intmax_t get_digits(char_u **pp)
 {
-  char_u *p = *pp;
-  long retval = atol((char *)p);
+  intmax_t number = strtoimax((char *)*pp, (char **)pp, 10);
+  assert(errno != ERANGE);
+  return number;
+}
 
-  if (*p == '-') {
-    // skip negative sign
-    ++p;
-  }
-  // skip to next non-digit
-  p = skipdigits(p);
-  *pp = p;
-  return retval;
+/// Get an int number from a string.
+///
+/// A get_digits wrapper restricted to int values.
+int get_int_digits(char_u **pp)
+{
+  intmax_t number = get_digits(pp);
+  assert(number >= INT_MIN && number <= INT_MAX);
+  return (int)number;
+}
+
+/// Get a long number from a string.
+///
+/// A get_digits wrapper restricted to long values.
+long get_long_digits(char_u **pp)
+{
+  intmax_t number = get_digits(pp);
+  assert(number >= LONG_MIN && number <= LONG_MAX);
+  return (long)number;
 }
 
 /// Return TRUE if "lbuf" is empty or only contains blanks.

src/nvim/cursor_shape.c
@@ -135,9 +135,7 @@ char_u *parse_shape_opt(int what)
               p += len;
               if (!VIM_ISDIGIT(*p))
                 return (char_u *)N_("E548: digit expected");
-              int64_t digits = getdigits(&p);
-              assert(digits <= INT_MAX);
-              int n = (int)digits;
+              int n = get_int_digits(&p);
               if (len == 3) {               /* "ver" or "hor" */
                 if (n == 0)
                   return (char_u *)N_("E549: Illegal percentage");

src/nvim/diff.c
@@ -1221,11 +1221,11 @@ static void diff_read(int idx_orig, int idx_new, char_u *fname)
     // {first}a{first}[,{last}]
     // {first}[,{last}]d{first}
     p = linebuf;
-    f1 = getdigits(&p);
+    f1 = get_long_digits(&p);
 
     if (*p == ',') {
       ++p;
-      l1 = getdigits(&p);
+      l1 = get_long_digits(&p);
     } else {
       l1 = f1;
     }
@@ -1235,11 +1235,11 @@ static void diff_read(int idx_orig, int idx_new, char_u *fname)
       continue;
     }
     difftype = *p++;
-    f2 = getdigits(&p);
+    f2 = get_long_digits(&p);
 
     if (*p == ',') {
       ++p;
-      l2 = getdigits(&p);
+      l2 = get_long_digits(&p);
     } else {
       l2 = f2;
     }
@@ -1783,7 +1783,7 @@ int diffopt_changed(void)
       diff_flags_new |= DIFF_FILLER;
     } else if ((STRNCMP(p, "context:", 8) == 0) && VIM_ISDIGIT(p[8])) {
       p += 8;
-      diff_context_new = getdigits(&p);
+      diff_context_new = get_int_digits(&p);
     } else if (STRNCMP(p, "icase", 5) == 0) {
       p += 5;
       diff_flags_new |= DIFF_ICASE;
@@ -1798,7 +1798,7 @@ int diffopt_changed(void)
       diff_flags_new |= DIFF_VERTICAL;
     } else if ((STRNCMP(p, "foldcolumn:", 11) == 0) && VIM_ISDIGIT(p[11])) {
       p += 11;
-      diff_foldcolumn_new = getdigits(&p);
+      diff_foldcolumn_new = get_int_digits(&p);
     }
 
     if ((*p != ',') && (*p != NUL)) {

src/nvim/digraph.c
@@ -1611,9 +1611,7 @@ void putdigraph(char_u *str)
       EMSG(_(e_number_exp));
       return;
     }
-    int64_t digits = getdigits(&str);
-    assert(digits <= INT_MAX);
-    int n = (int)digits;
+    int n = get_int_digits(&str);
 
     // If the digraph already exists, replace the result.
     dp = (digr_T *)user_digraphs.ga_data;

src/nvim/eval.c
@@ -2750,7 +2750,7 @@ void ex_lockvar(exarg_T *eap)
   if (eap->forceit)
     deep = -1;
   else if (vim_isdigit(*arg)) {
-    deep = getdigits(&arg);
+    deep = get_int_digits(&arg);
     arg = skipwhite(arg);
   }
 
@@ -13370,7 +13370,7 @@ static void f_setreg(typval_T *argvars, typval_T *rettv)
         yank_type = MBLOCK;
         if (VIM_ISDIGIT(stropt[1])) {
           ++stropt;
-          block_len = getdigits(&stropt) - 1;
+          block_len = get_long_digits(&stropt) - 1;
           --stropt;
         }
         break;

src/nvim/ex_cmds.c
@@ -569,7 +569,7 @@ void ex_retab(exarg_T *eap)
   save_list = curwin->w_p_list;
   curwin->w_p_list = 0;             /* don't want list mode here */
 
-  new_ts = getdigits(&(eap->arg));
+  new_ts = get_int_digits(&(eap->arg));
   if (new_ts < 0) {
     EMSG(_(e_positive));
     return;
@@ -3674,7 +3674,7 @@ void do_sub(exarg_T *eap)
    */
   cmd = skipwhite(cmd);
   if (VIM_ISDIGIT(*cmd)) {
-    i = getdigits(&cmd);
+    i = get_long_digits(&cmd);
     if (i <= 0 && !eap->skip && do_error) {
       EMSG(_(e_zerocount));
       return;
@@ -5920,7 +5920,7 @@ void ex_sign(exarg_T *eap)
 	arg1 = arg;
 	if (VIM_ISDIGIT(*arg))
 	{
-	    id = getdigits(&arg);
+	    id = get_int_digits(&arg);
 	    if (!vim_iswhite(*arg) && *arg != NUL)
 	    {
 		id = -1;
@@ -5985,7 +5985,7 @@ void ex_sign(exarg_T *eap)
 	    else if (STRNCMP(arg, "buffer=", 7) == 0)
 	    {
 		arg += 7;
-		buf = buflist_findnr((int)getdigits(&arg));
+		buf = buflist_findnr(get_int_digits(&arg));
 		if (*skipwhite(arg) != NUL)
 		    EMSG(_(e_trailing));
 		break;

Happy-Dude/vim

commit b580779a45d3854181c55cdb63382715614f4b20

fix #1839: decrement mouse row and col
- regression by 04c0658024a98a0586997f0ea8af1e3f774cc83e

src/nvim/term.c
@@ -3245,11 +3245,11 @@ int check_termcode(int max_offset, char_u *buf, int bufsize, int *buflen)
           if (key_name[0] == KS_SGR_MOUSE)
             mouse_code += 32;
 
-          mouse_col = getdigits_int(&p);
+          mouse_col = getdigits_int(&p) - 1;
           if (*p++ != ';')
             return -1;
 
-          mouse_row = getdigits_int(&p);
+          mouse_row = getdigits_int(&p) - 1;
           if (key_name[0] == KS_SGR_MOUSE && *p == 'm')
             mouse_code |= MOUSE_RELEASE;
           else if (*p != 'M')